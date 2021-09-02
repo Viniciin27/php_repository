36dbd6122daa2bb0a546c169af62e3c1e52d8d3f
Viniciin27 <89978077+Viniciin27@users.noreply.github.com>
2021-09-02 08:57:54 -0300

    Add files via upload

---
+++ atvd7.php
@@ -0,0 +1,27 @@
+<?php
+
+$jogador = " Pele"
+
+switch($jogador){
+
+    case "neymar" {
+        print ("Gol de ney");
+        break;
+    }
+
+    case "cr7" {
+        print ("Gol do cr7");
+        break;
+    }
+        
+     case "messi" {
+        print ("gol do messi");
+        break;
+     }   
+        
+     default:{
+         print ("Seu time está desfalcado amigo! :( ")
+     }
+
+}
+?>
